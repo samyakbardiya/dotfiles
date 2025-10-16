22032ea790cbcfa76a8270b14e70e8b39ebd54d7
Samyak Bardiya <mail@samyakbardiya.dev>
2025-10-16 19:06:19 +0530

    Update .config/homebrew/Brewfile Add .config/homebrew/new.Brewfile Update .config/karabiner/karabiner.json Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/overrides.lua Update .config/sketchybar/sketchybarrc

--- dot_config/nvim/lua/plugins/overrides.lua
+++ dot_config/nvim/lua/plugins/overrides.lua
@@ -1,5 +1,1 @@
-return {
-    -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
-    { "mason-org/mason.nvim", version = "1.11.0" },
-    { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
-}
+return {}
