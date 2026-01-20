31a42f40ee68d7c3446b3cfcb30a0eb4c2eefa71
Samyak Bardiya <mail@samyakbardiya.dev>
2026-01-20 15:20:50 +0530

    Fix SSH clipboard

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -20,3 +20,15 @@ vim.g.snacks_animate = false
 
 -- LazyExtras
 vim.g.lazyvim_picker = "fzf"
+
+-- Clipboard over SSH
+vim.o.clipboard = "unnamedplus"
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        -- vim.highlight.on_yank()
+        local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+        copy_to_unnamedplus(vim.v.event.regcontents)
+        local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+        copy_to_unnamed(vim.v.event.regcontents)
+    end,
+})
