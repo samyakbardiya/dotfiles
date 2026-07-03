200fded2bc685560c32383ca0d77299d69c95e40
Samyak Bardiya <mail@samyakbardiya.dev>
2026-07-03 15:53:02 +0530

    tmp: snacks picker ivy preset

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -7,11 +7,27 @@ return {
             top_down = false,
         },
         picker = {
+            preset = "ivy",
+            layout = {
+                height = 1,
+                width = 1,
+            },
+            -- stylua: ignore
+            keys = {
+                -- { "<leader>f/", LazyVim.pick("grep"), desc = "Grep (Root Dir)" },
+            },
             sources = {
                 ---@class snacks.picker.explorer.Config
                 explorer = {
                     -- auto_close = true,
                     hidden = true,
+                },
+            },
+            win = {
+                input = {
+                    keys = {
+                        ["<a-z>"] = { "toggle_maximize", mode = { "i", "n" } },
+                    },
                 },
             },
         },
